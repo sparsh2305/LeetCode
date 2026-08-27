e7496431af521da5c94df2ca23779e9b7ec5d496
Sparsh <68751861+sparsh2305@users.noreply.github.com>
2026-08-27 22:22:01 +0530

    Synchronize existing submission history (1/1)

---
+++ two-pointers/remove-element.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        # int num[]
+
+        # num.sort()
+
+        # int k == removeElement(nums, val)
+        # num.sort()
+
+        # for i in range(len(num)):
+            
+            ind =0 
+            for i in range(len(nums)):
+                if nums[i]!= val:
+                    nums[ind] = nums[i]
+                    ind+=1
+            return ind
+
