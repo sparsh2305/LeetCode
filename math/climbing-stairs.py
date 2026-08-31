faca8e6d7ec0b87e6fba7bae48571a857aed67ef
Sparsh <68751861+sparsh2305@users.noreply.github.com>
2026-08-31 23:50:26 +0530

    LC-climbing-stairs [Runtime: N/A; Memory: N/A]

--- math/climbing-stairs.py
+++ math/climbing-stairs.py
@@ -1,6 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 0 or n==1:
-            return 1
-        return self.climbStairs(n-1) + self.climbStairs(n-2)
+        a=1
+        b=1
+
+        for i in range(n):
+            a,b = b, a+b
+
+        return a
         
