51e1d2d854c57df2cf5ff0a44b63f3c14f44aed5
Sparsh <68751861+sparsh2305@users.noreply.github.com>
2026-09-07 23:41:27 +0530

    LC-power-of-two [Runtime: N/A; Memory: N/A]

--- recursion/power-of-two.py
+++ recursion/power-of-two.py
@@ -1,8 +1,7 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n== 1:
-            return True
-        if n%2 == 0:
-            return True
-        else:
+        if n <= 0:
             return False
+        while n % 2 == 0:
+            n = n // 2
+        return n == 1
